35d1676b200052dee214fc4ed2ac6ba2e1cc5623
ClydeFrog04 <egan.randal@gmail.com>
2019-06-12 22:18:45 -0600

    Added semi-complete camera control code

---
+++ Assets/Scripts/ObjectTypes/Player.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //This script is just used as a type to force certain object types in the inspector. Not entirely necessary but it ensures we don't accidentally put something other than a player on a script
+    //variable that calls for the player. I am using this in the CameraControl script currently
+}
