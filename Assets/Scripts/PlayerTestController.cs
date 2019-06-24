31d9ad8f9321c8c6bc6813cb0db4d62883eeaca2
spartann158 <ryandmiller3521@gmail.com>
2019-06-24 16:38:44 -0600

    added hook shoot and pull needs work

--- Assets/Scripts/PlayerTestController.cs
+++ Assets/Scripts/PlayerTestController.cs
@@ -28,6 +28,20 @@ public class PlayerTestController : MonoBehaviour
     private float straightShotSpeed;
     [SerializeField]
     private float shootDistance;
+    [SerializeField]
+    private GameObject firePos;
+    [SerializeField]
+    private float shotSpeed;
+    [SerializeField]
+    private GameObject pullHook;
+    [SerializeField]
+    private float returnDis;
+    private GameObject pullShot = null;
+   // private bool pullShotOut = false;
+    private bool pulling = false;
+    private bool returnPull = false;
+    private Vector3 pullPoint;
+    private bool pullHit = false;
 
 
     void Start(){
@@ -44,7 +58,10 @@ public class PlayerTestController : MonoBehaviour
 
 
     void Update(){
+        if(pullShot != null)//if the pull hook is in the game it will run all of the features of the hook depending on its current state
+            checkPullShot();
         movement();
+        Debug.Log(pullPoint);
         //test code
     }
 
@@ -64,10 +81,9 @@ public class PlayerTestController : MonoBehaviour
             //this is gravity falling. This can be disabled when we begin grappling and reapplied as needed.
             verticalVelocity -= gravity * Time.deltaTime;
         }
-
-        if(mainTarget != null){
-            moveTowards(targetHit.point);
-
+        if (pulling)
+        {
+            moveTowards(pullPoint);
         }
         if (Input.GetButtonUp("Fire1"))//this allows you to stick to the target spot until you release the mouse button also allows the player to stop the hook at any moment
         {
@@ -102,17 +118,77 @@ public class PlayerTestController : MonoBehaviour
             }
         }
 //fires the pull shot
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && pullShot == null)
         {
-            if(Physics.Raycast(this.transform.position, Camera.main.transform.forward,  out targetHit, shootDistance)){
+            //pullShotOut = true;
+            /*if(Physics.Raycast(this.transform.position, Camera.main.transform.forward,  out targetHit, shootDistance)){
                 mainTarget = targetHit.transform.gameObject;
+                gravityOn = false;
+                inputs = Vector3.zero;
+                GameManager.Instance.CanMove = false;
+            }*/
+            if (Physics.Raycast(this.transform.position, Camera.main.transform.forward, out targetHit, shootDistance))
+            {
+                mainTarget = targetHit.transform.gameObject;
+                pullHit = true;
+                pullPoint = targetHit.point;
+            }
+            //creates the pull shot prefab then rotates it 90 degrees so it is sitting in the right position
+            pullShot = Instantiate(pullHook, firePos.transform.position, Quaternion.identity) as GameObject;
+            pullShot.transform.Rotate(Camera.main.transform.rotation.x * 100, Camera.main.transform.rotation.y * 100, Camera.main.transform.rotation.z * 100);
+            pullShot.transform.Rotate(90, 0, 0);
+
+        }
+    }
+
+    private void checkPullShot()//used to fire and return the pull shot 
+    {
+        if(pullHit && !returnPull)//checks if the raycast sent out hit a target if it did and its not returning than fires the hook
+        {
+            Vector3 offset = pullPoint - pullShot.transform.position;
+            if(offset.magnitude > 0.1f)//moves the hook tell it hits the target
+            {
+                offset = offset.normalized * shotSpeed;
+                pullShot.transform.position = Vector3.MoveTowards(pullShot.transform.position, pullPoint, shotSpeed * Time.deltaTime);
+            }
+            else//once it hits the target it begings to pull the player toward the target
+            {
+                pulling = true;//once pulling is set to true the movetowards function will begin to be called
                 gravityOn = false;
                 inputs = Vector3.zero;
                 GameManager.Instance.CanMove = false;
             }
         }
-    }
+        else if(!returnPull && !pullHit)//if the raycast did not hit anything it will fire the hook out to the max distance than return back to the player
+        {
+            if(shootDistance > Vector3.Distance(pullShot.transform.position, this.transform.position))
+            {
+                pullShot.transform.Translate(Vector3.up * shotSpeed * Time.deltaTime);//moves the hook out from the players original spot its vector.up becuase the hook if rotated 90 degrees to look right
+            }
+            else
+            {
+                returnPull = true;
+            }
+        }
+        if(returnPull)//once the hook should return it will begin returning the hook
+        {
+            if (returnDis < Vector3.Distance(pullShot.transform.position, Camera.main.transform.position))
+            {
+                pullShot.transform.position = Vector3.MoveTowards(pullShot.transform.position, Camera.main.transform.position, shotSpeed * 2 * Time.deltaTime);
+            }
+            else
+            {
+                Destroy(pullShot);
+                pullShot = null;
+                pulling = false;
+                returnPull = false;
+                mainTarget = null;
+                pullHit = false;
+                //pullPoint = Vector3.zero;
+            }
+        }
 
+    }
     private void capVelocity(){
         inputs = Vector3.ClampMagnitude(inputs, maxSpeed);
     }
@@ -129,11 +205,14 @@ public class PlayerTestController : MonoBehaviour
     void arrivedAtTarget(){
         //will be used when the player arrives at the target, so anything control is take from the user and gravity is turned off, all of this gets put back when it needs to be
         mainTarget = null;
+        pulling = false;
         if (!Input.GetButton("Fire1"))
         {
             gravityOn = true;
+            //pulling = false;
             verticalVelocity = 0f;
             GameManager.Instance.CanMove = true;
+            returnPull = true;//sets the animation to return the pull
         }
     }
 
